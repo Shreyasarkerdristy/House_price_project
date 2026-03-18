4ed88451809885f25d8b8d229c9e2d9b02658c71
eric <eric@example.com>
2026-03-18 12:54:36 +0600

    fix model loading issue

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import util
 
 app = Flask(__name__)
+util.load_saved_artifacts()
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
@@ -34,7 +35,5 @@ import os
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
-
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
